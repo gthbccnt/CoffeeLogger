7cc9824755d6e7985653e5e334f49b688cae5ee7
gthbccnt <122404457+gthbccnt@users.noreply.github.com>
2023-05-16 12:14:53 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,9 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
-        '/index.html',
-        '/styles.css',
-        '/app.js'
+        'index.html',
+        'styles.css',
+        'app.js'
       ]);
     })
   );
